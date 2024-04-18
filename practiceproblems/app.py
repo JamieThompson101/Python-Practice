03295595b55cbc8d7c6ec85ddcea78d37eab1bfa
Jamie Thompson <130515437+JamieThompson101@users.noreply.github.com>
2024-04-17 22:15:43 -0400

    new print notes

--- practiceproblems/app.py
+++ practiceproblems/app.py
@@ -1,5 +1,22 @@
 name = input("What is your name?")
-print("Hello,")
-print(name)
 
 #most basic variable and function program by asking the user for their name
+
+#input only takes strings
+
+#when you insert multiple arguments to print, it will automatically add a space
+
+print("hello, ", end="" )
+
+print(name)
+
+#the print function can take any number of objects using the * syntax
+
+#by default, the print function ends with \n
+
+#print(*objects, sep=' ', end='\n', file=None, flush=False)
+
+#backslashes inside inner double quotes help the computer realize that those quotes dont finish a line referred to as "escaping"
+
+
+print("hello, \"friend\"")
