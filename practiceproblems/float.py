9cf80a0edf3138eb374ee0f997e10053ca5026d0
Jamie Thompson <130515437+JamieThompson101@users.noreply.github.com>
2024-04-19 23:38:05 -0400

    new float code

---
+++ practiceproblems/float.py
@@ -0,0 +1,21 @@
+#a float is an integer with a decimal more properly referred to as a floating point value
+
+#they are "real numbers"
+
+#use the float function instead of the int function to specify the variable's data type
+
+a = float(input("What is a? "))
+
+b = float(input("What is b? "))
+
+print(a + b)
+
+#use the round function to round the inputs to the nearest integer// the actual python docs for this function is round(number[, ndigits]) round --> number argument --> optional specification in brackets
+
+a = float(input("What is a? "))
+
+b = float(input("What is b? "))
+
+c = round(a + b)
+
+print(f"{c}")
