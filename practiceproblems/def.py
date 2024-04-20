b6eb43a55b7b6c07724e25b3db8d2e97d4de53c0
Jamie Thompson <130515437+JamieThompson101@users.noreply.github.com>
2024-04-20 19:41:58 -0400

    new examples for functions

--- practiceproblems/def.py
+++ practiceproblems/def.py
@@ -1,7 +1,10 @@
 #create functions using the key word def
 #add the parameter to//"Who do you want to say hello to?"// add the to in the print statement so the function says hello to that variable in name
-def hello(to):
+#you can set the parameter to to have a default value of "world" in the same line you're defining a function// just in case the user doesnt add a name
+def hello(to="world"):
     print("Hello,", to)
+#call hello 
+hello()
 name = input("What's your name? ")
 #call the function and give it the user's input variable as an argument
 hello(name)
